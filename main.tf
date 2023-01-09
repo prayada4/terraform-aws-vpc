8f0210ec5ec81597eb015e0e3063cd2bba352285
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-09 22:28:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "tfe-example"
