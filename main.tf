232cf07d6bff8bf9b06db32c1b5a1a81c3b4a545
prayada4 <121497330+prayada4@users.noreply.github.com>
2023-01-09 22:13:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "tfe-example"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tfe-example"
+  }
+}
